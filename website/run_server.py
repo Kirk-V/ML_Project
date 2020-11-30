72ecbb8b8d96099af312b475d383c99b058e836e
Christopher Steward <cstew764@gmail.com>
2020-11-30 10:49:32 -0500

    POSTing works, uploading and respond, work on seeking
    You can now upload a file to the server and get a response, just
    would need to hook that into the rest of our code.
    1. get file they uploaded
    2. do inference on it
    3. return with boxes, actor names, and probabilities
    4. stick that into HTML
    
    Still need to fix the seeking with the two bars to get the clip
    that they want out of the uploaded video. Will need to send those
    time codes with the data to the server.

--- website/run_server.py
+++ website/run_server.py
@@ -1,22 +1,43 @@
 #!/usr/bin/python3
 import http.server
 import socketserver
+from io import BytesIO
+import mimetypes
 
-class Handler(http.server.SimpleHTTPRequestHandler):    
-    def do_GET(self):
-        if self.path == "/get/get_result":
-            print("result")
-            #get result
-            return
-        
+import time
+
+#https://blog.anvileight.com/posts/simple-python-http-server/#do-post
+class Handler(http.server.SimpleHTTPRequestHandler):
+    count = 0
+    
+    def do_GET(self):        
         return http.server.SimpleHTTPRequestHandler.do_GET(self)
-            
+    
     def do_POST(self):
         if self.path == "/post/send_request":
-            print("request")
-            #get file
+            print("got request")
             
-    
+            content_length = int(self.headers["Content-Length"])
+            body = self.rfile.read(content_length)
+            file_type = self.headers["Content-type"]
+            file_ext = mimetypes.guess_extension(file_type);
+            self.send_response(200)
+            self.end_headers()
+            
+            f = open("./tmp/" + str(Handler.count) + file_ext, "wb")
+            Handler.count += 1
+            f.write(body);
+            f.close();
+            
+            #call inference function, return images, names, probabilities
+            #put into response format with JSON or XML
+
+            time.sleep(10)
+            
+            response = BytesIO()
+            response.write(b"This is a POST response.")
+            self.wfile.write(response.getvalue())
+            print("sent repsonse")
             
 def main():
     host = "127.0.0.1"
--- website/script.js
+++ website/script.js
@@ -1,80 +1,140 @@
 //https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/file
-const fileTypes = [
-    "image/jpeg",
-    "image/png",
-    "image/webp",
-    "image/gif",
+const video_file_types = [
     "video/mp4",
     "video/webm"
 ];
 
-function validFileType(file) {
-    return fileTypes.includes(file.type);
+const image_file_types = [
+    "image/jpeg",
+    "image/png",
+    "image/webp",
+    "image/gif"
+];
+
+//in seconds
+let video_length = null;
+let file = null;
+let file_type = null;
+
+function validFileType(type) {
+    if(video_file_types.includes(type)) {
+	return "video";
+    }
+    if(image_file_types.includes(type)) {
+	return "image";
+    }
+    return null;
 }
 
 function upload_file(that) {
+    video_length = null;
     const video = document.getElementById("video_container").getElementsByTagName("video")[0];
+    const image = document.getElementById("video_container").getElementsByTagName("img")[0];
 
     file = that.files[0];
+    file_type = validFileType(file.type)
     
-    if(validFileType(file.type)) {
+    if(file_type === null) {
 	console.log("unsupported file type")
 	return -1;
     }
 
-    video.setAttribute("src", URL.createObjectURL(file));
+    if(file_type === "video") {
+	video.style.display = "block";
+	image.style.display = "none";
+	video.setAttribute("src", URL.createObjectURL(file));
+	video.addEventListener('loadedmetadata', function() {
+	    setup(video.duration);	
+	});
+    }
+
+    if(file_type === "image") {
+	video.style.display = "none";
+	image.style.display = "block";
+	image.setAttribute("src", URL.createObjectURL(file));
+    }
 }
 
-function seek(start, end, event) {
-    const x = event.offsetX / this.offsetWidth;
-
-    //const value =;
-
+//https://www.simple.gy/blog/range-slider-two-handles/
+function seek(target, event) {
+    start_range = document.getElementById("start_range");
+    end_range = document.getElementById("end_range");
     
-    text = document.getElementById("start_range");
-    text = document.getElementById("end_range");
+    start_time = document.getElementById("start_time");
+    end_time = document.getElementById("end_time");
+
+    let pivot = null;
+    
+    if(target === start_range) {
+	if(start_range.valueAsNumber >= start_range.max) {
+	    pivot = Math.min(video_length, Number(start_range.max) );
+	}		
+    }
+    else if(target === end_range) {
+	if(end_range.valueAsNumber <= end_range.min) {
+	    pivot = Math.min(0, Number(end_range.min));
+	}
+    }
+
+    if(pivot != null) {
+	start_range.max = pivot;
+	end_range.min = pivot;
+    }
+    
+    start_range.style.flexGrow = Number(start_range.max) - Number(start_range.min);
+    end_range.style.flexGrow = Number(end_range.max) - Number(end_range.min);
+
+    start_time.value = start_range.value;
+    end_time.value = end_range.value;
+}
+
+function setup(length) {
+    video_length = length;
+    start = document.getElementById("start_range");
+    end = document.getElementById("end_range");
+
+    start_time = document.getElementById("start_time");
+    end_time = document.getElementById("end_time");
+    
+    start.min = 0;
+    start.max = video_length/2;
+    end.min = video_length/2;
+    end.max = video_length;
+
+    start_time.value = video_length*0.25;
+    end_time.value = video_length*0.75;
 }
 
 function send_request(that) {
     var httpRequest = new XMLHttpRequest();
-    var response = null;
+    httpRequest.open("POST", "/post/send_request", true);
+    httpRequest.setRequestHeader("Content-type", file.type);
+    httpRequest.responseType = 'text';
+        
     httpRequest.onreadystatechange = function() {
-	if(httpRequest.readyState == 4 && httpRequest.status == 200) {
-	    response = httpRequest.responseText;
-	    callback(JSON.parse(response));
+	if(httpRequest.readyState === 4 && httpRequest.status === 200) {
+	    process_response(httpRequest.responseText);
 	}
     };
     
-    httpRequest.open("POST", "/post/send_request", true);
-    httpRequest.send();
+    httpRequest.send(file);
     
-    wait_request();
 }
 
-function wait_request() {
-    var httpRequest = new XMLHttpRequest();
-    var response = null;
-    httpRequest.onreadystatechange = function() {
-	if(httpRequest.readyState == 4 && httpRequest.status == 200) {
-	    response = httpRequest.responseText;
-	    callback(JSON.parse(response));
-	}
-    };
-    
-    httpRequest.open("GET", "/get/get_result", true);
-    httpRequest.send();
+function process_response(response) {
+    output = document.getElementById("output");
+    output.innerHTML = response;
 }
 
 function main() {
     start = document.getElementById("start_range");
     end = document.getElementById("end_range");
     
-    start.onmousemove = function(e) {seek(start, end, event)};
-    end.onmousemove = function(e) {seek(start, end, event)};
+    start_range.onmousemove = function(e) {seek(start, event)};
+    end_range.onmousemove = function(e) {seek(end, event)};
     
     return 0;
 }
-
 
 window.addEventListener("DOMContentLoaded", (event) => {
     main();
