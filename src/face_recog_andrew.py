3c1e8171be73ac2e38891a96aff1b0a4f3d3b00b
Andrew <andrewvk7@gmail.com>
2020-12-02 23:11:56 -0500

    Working on image loading

--- src/face_recog_andrew.py
+++ src/face_recog_andrew.py
@@ -6,24 +6,62 @@ import cv2
 
 # For loading pretrained model
 # model = load_model('model.h5')
-# summarize model.
+# summarize model
 # model.summary()
 
 # freezing last 4 layers of a keras model
-# for layers in vgg.layers:
-#     layer.trainable = False
+for layers in vgg.layers:
+    layer.trainable = False
 
-img_rows, img_cols = 244, 2444
 vgg = VGG16(weights = 'imagenet',
             include_top = False,
-            input_shape = (img_rows, img_cols, 3))
+            input_shape = (224, 224, 3))
 
-# Gets the ype of layer and if they're trainable
+# Gets the type of layer and if they're trainable
 for (i, layer) in enumerate(vgg.layers):
     print(str(i) + " " + layer.__class__.__name__, layer.trainable)
 
-# from keras.models import Sequential
-# from keras.layers import Dense, Dropout, Activation, Flatten, GlobalAveragePooling2D
-# from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
-# from keras.layers.normalization import BatchNormalization
-# from keras.models import Model
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten, GlobalAveragePooling2D
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.losses import SparseCategoricalCrossentropy
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+
+dir = "."
+
+AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+
+## TODO get data from directory
+
+checkpoint = ModelCheckpoint("face_vgg.h5",
+                             monitor="val_loss",
+                             mode="min",
+                             save_best_only = True,
+                             verbose=1)
+earlystop = EarlyStopping(monitor = 'val_loss',
+                            min_delta = 0,
+                            patience = 3,
+                            verbose = 1,
+                            restore_best_weights = True)
+
+# we put our call backs into a callback list
+callbacks = [earlystop, checkpoint]
+
+model.compile(loss = losses.SparseCategoricalCrossentropy(from_logits=True),
+                optimizer = 'adam', # may want something with learning rate
+                metrics = ['accuracy'])
+
+# amounts for train/test split per actor
+num_train = 20
+num_test = 20
+
+# Training
+epochs = 10
+batch_size = 64
