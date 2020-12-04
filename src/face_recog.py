60bfdbe041a1172bab569ad915bd21388a8e4486
Kirk Vander Ploeg <kvande85@uwo.ca>
2020-12-04 13:35:07 -0500

    cropping for detected faces. Replaces old images in detectedImages folder

--- src/face_recog.py
+++ src/face_recog.py
@@ -3,7 +3,7 @@ import sys
 
 from frame_extractor import FrameExtractor
 from boxes import BoxMaker
-from detect import detect_object
+from detect import detect_object, crop
 
 
 
@@ -11,8 +11,6 @@ from detect import detect_object
 
 # useful links 
 # Captureing frames from vid: https://docs.opencv.org/3.4/d8/dfe/classcv_1_1VideoCapture.html#a473055e77dd7faa4d26d686226b292c1
-
-
 
 """
 	This file shows the usage of frameExtractor, and detection. 
@@ -31,8 +29,8 @@ from detect import detect_object
 
 """
 
-#make frame extractor object
-framer = FrameExtractor('videos/Silicon Valley S01E02.mp4', 1000)
+# #make frame extractor object
+framer = FrameExtractor('../videos/The King of Staten Island.mp4', 5000)
 
 # extract and store frames in imagesToDetect
 framer.store_frames('imagesToDetect')
@@ -45,3 +43,5 @@ prediction = detect_object()
 # show boxes and confidence for each image
 print(prediction)
 
+#testing cropper
+crop(prediction, './detectedImages',)
