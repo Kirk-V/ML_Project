4b988b4d7cb02e20834e5b6f7335fad232d7497a
Kirk Vander Ploeg <kvande85@uwo.ca>
2020-12-06 22:38:31 -0500

    classification.py cleanup

--- src/classification.py
+++ src/classification.py
@@ -4,29 +4,32 @@ import tensorflow as tf
 from keras.models import load_model
 
 def face_recognition():
+    """
+    function to classify images within the croppedImages directory
+    ...
+
+    returns: 
+      conf: list
+        conf is list of tuples; tuple is (filename, [confidence list])
+
+    """
 
     gpu_options = tf.compat.v1.GPUOptions(allow_growth=True)
     session = tf.compat.v1.InteractiveSession(config=tf.compat.v1.ConfigProto(gpu_options=gpu_options))
 
-    classifier = load_model('face_vgg.h5')
+    classifier = load_model('resnet_weights.h5')
     conf = []
 
     os.chdir("./croppedImages/")
     for file in glob.glob("*.jpg"):
-        # input_im = cv2.imread("./detectedImages/0220px-JayBaruchel08TIFF.jpg")
         input_im = cv2.imread(file)
-
-        # cv2.imshow('image', input_im)
         input_original = input_im.copy()
         input_original = cv2.resize(input_original, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_LINEAR)
         input_im = input_im / 255.
         input_im = cv2.resize(input_im, (224, 224), 3, interpolation = cv2.INTER_LINEAR)
         input_im = input_im.reshape(1,224,224,3)
-        # cv2.imshow('image', input_im)
-        # Get Prediction
         res = classifier.predict(input_im, 1, verbose = 0)
         conf.append([file, res])
 
     os.chdir("..")
-    # conf is list of tuples; tuple is (filename, confidence list)
     return conf
